6624fc7fa4174afc1037e20a7a766ed7d94a1f97
Williams <williams.422024010@civitas.ukrida.ac.id>
2025-09-25 13:49:58 +0700

    Module Product insert data to table products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-//import model product
+
 use App\Models\Product;
-
-//import return type View
 use Illuminate\View\View;
-
 use Illuminate\Http\Request;
+use App\Models\Category_product;
+use App\Models\Supplier;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -26,4 +27,57 @@ class ProductController extends Controller
         //render view with products
         return view('products.index', compact('products'));
     }
+
+    /**
+     * create
+     *
+     * @return View
+     */
+    public function create(): View
+    {
+        $product = new Category_product;
+        $product2 = new Supplier;
+
+        $data['categories'] = $product->get_category_product()->get();
+        $data['suppliers'] = $product2->get_supplier()->get();
+
+        return view('products.create', compact('data'));
+    }
+
+        /**
+     * store
+     *
+     * @param  mixed $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        // var_dump($request);exit;
+        //validate form
+        $validatedData = $request->validate([
+            'image'             => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'             => 'required|min:5',
+            'product_category_id' => 'required|integer',
+            'supplier_id'        => 'required|integer',
+            'description'       => 'required|string',
+            'price'             => 'required|numeric',
+            'stock'             => 'required|numeric'
+        ]);
+
+        // Menangani upload file gambar
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $store_image = $image->store('images', 'public'); // Simpan gambar ke folder penyimpanan
+
+            $product = new Product;
+            $insert_product = $product->storeProduct($request, $image);
+
+            //redirect to index
+            return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
+        }
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['error' => 'Failed to upload image (request).']);
+    }
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,22 +4,51 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Product extends Model
 {
+
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'image',
+        'title',
+        'product_category_id',
+        'supplier_id',
+        'description',
+        'price',
+        'stock',
+        'supplier_id' // tambahin juga kalau nanti mau mass assign supplier
+    ];
+
     public function get_product()
-{
-    //get all products
-    $sql = $this->select(
-        "products.*", 
-        "category_product.product_category_name as product_category_name",
-        "supplier.supplier_name as supplier_name"
+    {
+        // get all products
+        $sql = $this->select(
+            "products.*", 
+            "category_product.product_category_name as product_category_name",
+            "supplier.supplier_name as supplier_name"
         )
-        ->join('category_product', 'category_product.id', '=', 'products.product_category_id')
-        ->join('supplier', 'supplier.id', '=', 'products.supplier_id');
+        ->leftjoin('category_product', 'category_product.id', '=', 'products.product_category_id')
+        ->leftjoin('supplier', 'supplier.id', '=', 'products.supplier_id');
 
-    return $sql;
+        return $sql;
+    }
+        public static function storeProduct($request, $image)
+        {
+        // Simpan produk baru menggunakan mass assignment
+        return self::create([
+            'image'               => $image->hashName(),
+            'title'               => $request->title,
+            'product_category_id' => $request->product_category_id,
+            'supplier_id'         => $request->supplier_id,
+            'description'         => $request->description,
+            'price'               => $request->price,
+            'stock'               => $request->stock,
+        ]);
+    }
 }
-
-}
-
-
