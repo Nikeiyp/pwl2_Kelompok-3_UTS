3bc6ee714496d7aaffdd75af34751a5f58230ccb
hatchiisoo <turtl3rabb1t0121@gmail.com>
2025-10-08 09:05:02 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//route resource for products
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
-
-Route::resource('/suppliers', \App\Http\Controllers\SupplierController::class);
